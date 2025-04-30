2c7cc542dce7f2ed142ab5c695470ea6a7efc51e
gujjar-302 <gujjarjawad7875@gmail.com>
2025-04-30 16:00:06 +0200

    Primeraweb

---
+++ scripts.js
@@ -0,0 +1,4 @@
+document.querySelector('form').addEventListener('submit', function(event) {
+    event.preventDefault();  // Evita el envío del formulario
+    alert('Gracias por contactarnos, ¡nos pondremos en contacto contigo pronto!');
+});
